1ecc9394d67e7e187c4328a9259ff16c5fec37c9
Kyle Coberly <kyle.coberly@gmail.com>
2021-06-26 11:04:50 -0600

    Initial commit

---
+++ data/teams.js
@@ -0,0 +1,21 @@
+module.exports = [{
+    id: 1,
+    teamName: "The Blue Falcons",
+    teamCode: "bf",
+    numberOfPlayers: 28
+},{
+    id: 2,
+    teamName: "The Screaming Eagles",
+    teamCode: "se",
+    numberOfPlayers: 24
+},{
+    id: 3,
+    teamName: "The Wild Raptors",
+    teamCode: "wr",
+    numberOfPlayers: 30
+},{
+    id: 4,
+    teamName: "The Killer Hogs",
+    teamCode: "kh",
+    numberOfPlayers: 29
+}];
